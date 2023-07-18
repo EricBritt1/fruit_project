d6d092a27549d4f777957e2e9d70437590d2bb22
EricBritt1 <britteric055@gmail.com>
2023-07-18 13:10:59 -0700

    push for TA help (unfinished)

---
+++ FruitProject.js
@@ -0,0 +1,81 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+function search(str) {
+	
+	let results = [];
+	// TODO
+	fruit.filter(function (word) {
+		let wordToLowerCase = word.toLocaleLowerCase()
+		if(wordToLowerCase.includes(str) === true) {
+			return results.push(word)
+		}
+	}) 
+	return results
+};
+
+function searchHandler(e) {
+	let userInput = `${e.target.value}`;
+
+	if(userInput.length > 0) {
+		let updatedReturn = search(userInput);
+		showSuggestions(updatedReturn, userInput)
+	};
+	if(userInput.length === 0) {
+		let newUpdatedReturn = search('jhvvvhjvjhv');
+		showSuggestions(newUpdatedReturn, userInput)
+	};
+	// else if(e.target.value = '') {
+	// !showSuggestions(updatedReturn,userInput)
+	// }
+	
+};
+
+function showSuggestions(results, inputVal) {
+
+	suggestions.innerText = '';
+	results.filter(function (word) {
+		let li = document.createElement('li')
+		li.innerText = word.toLowerCase();
+		suggestions.append(li)
+	})
+}
+
+// TODO
+
+
+function useSuggestion(e) {
+	if(e.target.tagName === 'LI' ) {
+		input.value = e.target.innerText
+		suggestions.innerText = '';
+	}
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
+
+// if(inputVal.length < 1) {
+// 	if(suggestions.hasChildNodes()) {
+// 		results = []
+// 	}
+// }
+// else if(inputVal.length === 1) {
+// 	results.filter(function (word) {
+// 		let li = document.createElement('li')
+// 		li.innerText = word;
+// 		suggestions.append(li)
+// 	})
+// }
+// else if (inputVal.length > 1) {
+// 	if(suggestions.hasChildNodes()){
+// 		suggestions.innerHTML = '';
+// 	}
+// 	results.filter(function (word) {
+// 		let li = document.createElement('li')
+// 		li.innerText = word;
+// 		suggestions.append(li)
+// 	})
+// }
